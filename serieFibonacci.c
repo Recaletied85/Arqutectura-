2ffcc6be97a157844f7998b7155097fdfad0602a
Emilio <emilio.xdf@gmail.com>
2026-08-21 13:59:32 +0000

    fibonnacci 1

---
+++ serieFibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	int x=0, j=1,m=0;
+		
+	printf("-------------Generador serie fibonacci---------");
+	printf("-------Dame un numero para la serie fibonacci: ");
+	scanf("%d",&x);
+
+	for (int i=0; i<x; i++){
+		printf("%d, %d,  ",m,j);
+		m += j;
+		j += m;
+		
+	}
+
+	return 0;
+
+}
